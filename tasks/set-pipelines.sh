de6ed6d7f805b6c099abef4dd2800f4bef52d9d2
Szymon Janota <szymon.janota@relayr.io>
2017-07-25 20:41:36 +0200

    sjanota: set configs

--- tasks/set-pipelines.sh
+++ tasks/set-pipelines.sh
@@ -4,6 +4,13 @@ set -e
 set -x
 
 fly -t target login -c "${CONCOURSE_URL}" $CONCOURSE_LOGIN
-fly -t target pipelines
+PIPELINES=$(fly -t target pipelines | tail -n +1 | awk '{print $1}' | grep -v "${SELF_UPDATE_NAME}")
+echo "$PIPELINES"
 
-ls ci-src/pipelines
+pushd ci-src/pipelines >/dev/null
+for config in $(ls); do
+  NAME=$(echo $config | awk -F. '{print $1}')
+  echo $NAME
+  fly -t target set-pipeline -p $NAME -c $config
+done
+popd >/dev/null
