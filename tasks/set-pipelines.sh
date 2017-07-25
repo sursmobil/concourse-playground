046ebf977e769aafecd2b47996bc4f73290edf59
Szymon Janota <szymon.janota@relayr.io>
2017-07-25 21:01:09 +0200

    sjanota: fix lsiting pipelines

--- tasks/set-pipelines.sh
+++ tasks/set-pipelines.sh
@@ -4,13 +4,14 @@ set -e
 set -x
 
 fly -t target login -c "${CONCOURSE_URL}" $CONCOURSE_LOGIN
-PIPELINES=$(fly -t target pipelines | tail -n +1 | awk '{print $1}' | grep -v "${SELF_UPDATE_NAME}")
-echo "$PIPELINES"
+
+## echo at the end is to suppress grep error on no output
+PIPELINES=$(fly -t target pipelines | tail -n +1 | awk '{print $1}' | grep -v "${SELF_UPDATE_NAME}" | xargs echo)
 
 pushd ci-src/pipelines >/dev/null
 for config in $(ls); do
   NAME=$(echo $config | awk -F. '{print $1}')
   echo $NAME
-  fly -t target set-pipeline -p $NAME -c $config
+  fly -t target set-pipeline -p $NAME -c $config -n
 done
 popd >/dev/null
