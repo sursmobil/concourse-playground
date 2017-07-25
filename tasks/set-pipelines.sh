3f7be767a49a906c21886d20bff945cf2306725e
Szymon Janota <szymon.janota@relayr.io>
2017-07-25 20:07:09 +0200

    sjanota: get fly

--- tasks/set-pipelines.sh
+++ tasks/set-pipelines.sh
@@ -5,5 +5,7 @@ set -x
 
 echo "Yupi!"
 apt-get update
-apt-get install -y iputils-ping
-ping ${CONCOURSE_URL} -c 1
+apt-get install -y wget
+wget http://192.168.100.4:8080/api/v1/cli?arch=amd64&platform=linux -O fly
+chmod +x ./fly
+./fly --help
