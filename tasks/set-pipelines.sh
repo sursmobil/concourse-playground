dd64010b153b5998c5a2a91cb4fa3f4ccab9b2e6
Szymon Janota <szymon.janota@relayr.io>
2017-07-25 20:29:58 +0200

    sjanota: use fly from path

--- tasks/set-pipelines.sh
+++ tasks/set-pipelines.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-./fly -t target login -c "${CONCOURSE_URL}" $CONCOURSE_LOGIN
-./fly -t target pipelines
+fly -t target login -c "${CONCOURSE_URL}" $CONCOURSE_LOGIN
+fly -t target pipelines
