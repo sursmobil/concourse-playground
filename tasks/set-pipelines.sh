86f5fabc7aed850cb7cd62161af6bec62d443888
Szymon Janota <szymon.janota@relayr.io>
2017-07-25 20:16:39 +0200

    sjanota: fly login

--- tasks/set-pipelines.sh
+++ tasks/set-pipelines.sh
@@ -3,9 +3,10 @@
 set -e
 set -x
 
-echo "Yupi!"
 apt-get update
 apt-get install -y wget
 wget "${CONCOURSE_URL}/api/v1/cli?arch=amd64&platform=linux" -O fly
 chmod +x ./fly
-./fly --help
+
+./fly -t target login -c "${CONCOURSE_URL}" $CONCOURSE_LOGIN
+./fly -t target pipelines
