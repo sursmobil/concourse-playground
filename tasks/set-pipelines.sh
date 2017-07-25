6d21a2b0f0627c1d52e5402c1f281da7bd019e06
Szymon Janota <szymon.janota@relayr.io>
2017-07-25 20:09:26 +0200

    sjanota: fix wget call

--- tasks/set-pipelines.sh
+++ tasks/set-pipelines.sh
@@ -6,6 +6,6 @@ set -x
 echo "Yupi!"
 apt-get update
 apt-get install -y wget
-wget http://192.168.100.4:8080/api/v1/cli?arch=amd64&platform=linux -O fly
+wget "${CONCOURSE_URL}/api/v1/cli?arch=amd64&platform=linux" -O fly
 chmod +x ./fly
 ./fly --help
