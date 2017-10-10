14f9dc5e46058dc5e4f8035761927b0de8a4a5e5
Szymon Janota <szymon.janota@relayr.io>
2017-10-10 12:52:18 +0200

    sjanota: revert changes in set-pipelines

--- tasks/set-pipelines.sh
+++ tasks/set-pipelines.sh
@@ -7,15 +7,12 @@ fly -t target login -c "${CONCOURSE_URL}" $CONCOURSE_LOGIN
 
 ## echo at the end is to suppress grep error on no output
 PIPELINES=$(fly -t target pipelines | tail -n +1 | awk '{print $1}' | grep -v "${SELF_UPDATE_NAME}" | xargs echo)
-function load_configs() {
-  local dir=$1
-  for config in $(ls $dir); do
-    NAME=$(echo $config | awk -F. '{print $1}')
-    echo $NAME
-    fly -t target set-pipeline -p $NAME -c $dir/$config -n
-    fly -t target unpause-pipeline -p $NAME
-  done
-}
 
-load_config ci-src
-load_config ci-gen
+pushd ci-src/pipelines >/dev/null
+for config in $(ls); do
+  NAME=$(echo $config | awk -F. '{print $1}')
+  echo $NAME
+  fly -t target set-pipeline -p $NAME -c $config -n
+  fly -t target unpause-pipeline -p $NAME
+done
+popd >/dev/null
