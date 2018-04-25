14e34c90f62731912109b2a0f3b8d8fb400d966d
marcinrejdych <marcin.rejdych@relayr.io>
2018-04-25 14:10:45 +0200

    self update uses pipeline-resource

---
+++ tasks/collect-pipelines.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+mkdir pipelines
+cp ci-gen/* pipelines
+cp ci-src/pipelines/* pipelines
+
+pushd pipelines >/dev/null
+
+touch pipelines.yaml
+echo "pipelines" > pipelines.yaml
+
+for config in $(ls); do
+  NAME=$(echo $config | awk -F. '{print $1}')
+  CONFIG_FILE="pipelines/$config"
+
+  set +x
+  echo "- name: $NAME" >> pipelines.yaml
+  echo "  team: $TEAM" >> pipelines.yaml
+  echo "  config_file: $CONFIG_FILE" >> pipelines.yaml
+  echo "  unpaused: true" >> pipelines.yaml
+  set -x
+
+done
+
+cat pipelines.yaml
+
+popd >/dev/null
